9fce53a6541f444d855d9f1266670674925f210a
hyemiJ <166362402+hyemiJ@users.noreply.github.com>
2024-12-03 13:16:03 +0900

    Serviet interface 와 GenericServlet ,HttpServlet 클래스를 알아본다

--- servlet-pratice/src/main/java/org/example/CalculatorServlet.java
+++ servlet-pratice/src/main/java/org/example/CalculatorServlet.java
@@ -6,6 +6,7 @@ import org.example.calculate.PositiveNumber;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import javax.servlet.GenericServlet;
 import javax.servlet.Servlet;
 import javax.servlet.ServletConfig;
 import javax.servlet.ServletException;
@@ -19,38 +20,63 @@ import java.io.IOException;
 import java.io.PrintWriter;
 
 @WebServlet("/calculate")
-public class CalculatorServlet implements Servlet {
+public class CalculatorServlet extends HttpServlet {
 	private static final Logger log = LoggerFactory.getLogger(CalculatorServlet.class);
+	private ServletConfig config;
 
+//	@Override
+//	public void init(ServletConfig config) throws ServletException {
+//		log.info("init");
+//		this.config = config;
+//		
+//	}//implements Servlet
+
+//	@Override
+//	public void service(ServletRequest request, ServletResponse response) throws ServletException, IOException {
+//		log.info("service");
+//		
+//		//v1 . service 로직 실행
+//		int a = Integer.parseInt(request.getParameter("a"));
+//		String operator = request.getParameter("operator");
+//		int b = Integer.parseInt(request.getParameter("b"));
+//		int result = CalCulator.calculateInterface2(new PositiveNumber(a), operator, new PositiveNumber(b));
+//		
+//		PrintWriter writer = response.getWriter();
+//		writer.println(result); // http://localhost:8080/servlet-pratice/calculate?a=10&operator=*&b=10 -> 결과 100 이 출력
+//	}//extends GenericServlet
+
+//	@Override
+//	public void destroy() {
+//		log.info("destroy");
+//		// 자원 해제 
+//		
+//	}//implements Servlet
+//
+//	@Override
+//	public ServletConfig getServletConfig() {
+//		return this.config;
+//	}//implements Servlet
+//	
+//	@Override
+//	public String getServletInfo() {
+//		// TODO Auto-generated method stub
+//		return null;
+//	}//implements Servlet
+//	
 	@Override
-	public void init(ServletConfig config) throws ServletException {
-		log.info("init");
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		//v 2 . service 로직 실행
+		int a = Integer.parseInt(request.getParameter("a"));
+		String operator = request.getParameter("operator");
+		int b = Integer.parseInt(request.getParameter("b"));
+		int result = CalCulator.calculateInterface2(new PositiveNumber(a), operator, new PositiveNumber(b));
 		
-	}
-
-	@Override
-	public void service(ServletRequest req, ServletResponse res) throws ServletException, IOException {
-		log.info("service");
-		
-	}
-
-	@Override
-	public void destroy() {
-		log.info("destroy");
-		
-	}
-
-	@Override
-	public ServletConfig getServletConfig() {
-		// TODO Auto-generated method stub
-		return null;
-	}
-	
-	@Override
-	public String getServletInfo() {
-		// TODO Auto-generated method stub
-		return null;
-	}
+		PrintWriter writer = response.getWriter();
+		writer.println(result); 
+		// http://localhost:8080/servlet-pratice/calculate?a=10&operator=*&b=10 -> 결과 100 이 출력
+		//http://localhost:8080/servlet-pratice/calculate?a=10&operator=%2b&b=10 -> 결과 20이 출력
+		// + 는 url 로 예약어를 의미하기 떄문에 인코딩이 필요하다.
+	}//extends HttpServlet
 	
 
 }
