5563776c822a36dbeb4f75cf4e892fd52ead1639
hyemiJ <166362402+hyemiJ@users.noreply.github.com>
2024-12-09 14:01:57 +0900

    프런트 컨트롤러 패턴실습
    직접 DispatcherServlet클래스를 만들어보고 Controller인터페이스를 정의하여  DispatcherServlet 클래스를 활용하여 해당뷰를 리턴해보는 FrontController 패턴을 실습해본다.

--- mvc-practive/src/main/java/org/example/mvc/DispatcherServlet.java
+++ mvc-practive/src/main/java/org/example/mvc/DispatcherServlet.java
@@ -1,8 +1,10 @@
 package org.example.mvc;
 
+import org.example.mvc.controller.Controller;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -15,8 +17,37 @@ import java.io.IOException;
 public class DispatcherServlet extends HttpServlet {
     private static final Logger log = LoggerFactory.getLogger(DispatcherServlet.class);
 
+    private RequestMappingHandlerMapping requestMappingHandlerMapping;
+
+    //tomcat 이 HttpServlet을 싱글톤 , 객체 1개로 만드는데
+    //그때 서블릿이 만들어지면서 아래의 init 메소드가 호출된다.
+    //requestMappingHandlerMapping이
+    /*
+        void init(){
+        mappings.put("/", new HomeController());
+        // 어떤 경로가 없을경우 HomeController 실행
+    }
+     */
+    // 해당 메서드를 실행하면서 Map을 초기화 하도록 하였다.
+    @Override
+    public void init() throws ServletException {
+       //초기화
+        requestMappingHandlerMapping = new RequestMappingHandlerMapping();
+        requestMappingHandlerMapping.init();
+    }
+
     @Override
     protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         log.info("Dispatching request...");
+        Controller handler = requestMappingHandlerMapping.findController(request.getRequestURI());
+        //요청 uri에 대한 작업을 service로 위임.
+        try {
+            String viewname = handler.handleRequest(request,response);
+            log.info("viewname = {}", viewname);
+            RequestDispatcher dispatcher = request.getRequestDispatcher(viewname);
+            dispatcher.forward(request, response);
+        } catch (Exception e) {
+            log.error(e.getMessage());
+        }
     }
 }
--- mvc-practive/src/main/java/org/example/mvc/RequestMappingHandlerMapping.java
+++ mvc-practive/src/main/java/org/example/mvc/RequestMappingHandlerMapping.java
@@ -1,6 +1,7 @@
 package org.example.mvc;
 
 import org.example.mvc.controller.Controller;
+import org.example.mvc.controller.HomeController;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -9,4 +10,15 @@ public class RequestMappingHandlerMapping {
     private Map<String , Controller> mappings = new HashMap<>();
     //key : url path / value : 해당 controller
     //만약 "/user" -> userController 를 호출해달라 라는 의미
+
+    void init(){
+        mappings.put("/", new HomeController());
+        // 어떤 경로가 없을경우 HomeController 실행
+    }
+
+    public Controller findController(String urlpath){
+        return mappings.get(urlpath);
+        //urlPath 와 일치하는 컨트롤러를 리턴해주는 메소드
+    };
+
 }
